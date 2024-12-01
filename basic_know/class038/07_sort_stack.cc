946565e77d7353809f1733418df3ad5bfeea29d3
gexin.coder <gexincoding@gmail.com>
2024-12-01 21:11:19 +0800

    stack递归排序

--- basic_know/class038/07_sort_stack.cc
+++ basic_know/class038/07_sort_stack.cc
@@ -5,22 +5,46 @@ using namespace std;
 
 stack<int> s;
 
-int s_max(int deep) { return 0; }
+int s_max(int deep) {
+  if (deep == 0) return INT_MIN;
+  int t = s.top();
+  s.pop();
+  int res = s_max(deep - 1);
+  if (t > res) res = t;
+  s.push(t);
+  return res;
+}
 
-int get_times(int deep, int num) { return 0; }
+int get_times(int deep, int num) {
+  if (deep == 0) return 0;
+  int t = s.top();
+  s.pop();
+  int res = get_times(deep - 1, num);
+  if (t == num) res++;
+  s.push(t);
+  return res;
+}
 
-void down(int deep, int max) {}
-
-void sort(int deep) {
-  if (s.empty()) {
+void down(int deep, int max, int time) {
+  if (deep == 0) {
+    for (int i = 0; i < time; i++) s.push(max);
     return;
   }
-  // 1. max
-  int max = s_max(deep);
-  // 2. time
-  int time = get_times(deep, max);
-  down(deep, max);
-  sort(deep - time);
+  int t = s.top();
+  s.pop();
+  down(deep - 1, max, time);
+  if (max != t) s.push(t);
+}
+
+void sort() {
+  if (s.empty()) return;
+  int deep = s.size();
+  while (deep > 0) {
+    int max = s_max(deep);
+    int time = get_times(deep, max);
+    down(deep, max, time);
+    deep -= time;
+  }
 }
 
 int main() {
@@ -31,7 +55,7 @@ int main() {
     cin >> c;
     s.push(c);
   }
-  sort(s.size());
+  sort();
   while (!s.empty()) {
     cout << s.top() << " ";
     s.pop();
